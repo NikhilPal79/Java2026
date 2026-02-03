da710440e38eb1568b6b3efadf32a9ccf8741ae9
NikhilPal79 <nikhilchpr7@gmail.com>
2026-02-03 13:04:34 -0500

    8feb

---
+++ src/main/java/Project/feb08.java
@@ -0,0 +1,12 @@
+package Project;
+
+public class feb08 {
+
+    static void main() {
+        System.out.println("");
+        System.out.println("Hello Nikhil all the best for the revision ");
+        System.out.println("i have learnt two method of doing coding." +
+                "SOFTWARE DEVELOPMENT LIFE CYCLE SDLC = 1. WATERFALL METHOD " +
+                "SOFTWARE TESTING LIFE CYCLE  STLC    = 2.  ");
+    }
+}
