ee1c70f08a9ff2d911b3b574bb0e8efa0bd2de49
NikhilPal79 <nikhilchpr7@gmail.com>
2026-02-22 18:12:32 -0500

    for loop

--- src/main/java2026/inclass/feb18/ForLoop.java
+++ src/main/java2026/inclass/feb18/ForLoop.java
@@ -11,20 +11,24 @@ public class ForLoop {
         System.out.println("enter a number");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int sum = 0;
 
+        for  (int x =  2; x <= n; x = x + 2) {
+            sum = sum + x;
 
-        for  (int i = 1; i <= n; i++) {
-            System.out.println(i);
         }
-
-
-
-
-
-
+        System.out.println(sum );
 
 
         /// sum of first n odd number
 
+        System.out.println("enter a number");
+        int m = sc.nextInt();
+        int sum2 = 0;
+
+        for  (int x = 1; x <= m; x = x + 2) {
+            sum2 = sum2 + x;
+        }
+        System.out.println(sum2 );
     }
 }
