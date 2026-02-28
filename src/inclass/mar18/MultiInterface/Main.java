0b5929487c21ed2dec260415fb5aa63748d5e983
NikhilPal79 <nikhilchpr7@gmail.com>
2026-02-28 14:54:27 -0500

    inteface and classes with use of extends and implements

--- src/inclass/mar18/MultiInterface/Main.java
+++ src/inclass/mar18/MultiInterface/Main.java
@@ -3,7 +3,14 @@ package inclass.mar18.MultiInterface;
 public class Main {
     static void main(String[] args) {
 
-        Smartphone smartphone = new Smartphone();
-        smartphone.getMultiFeatures();
+        Samsung samsung = new Samsung();
+        samsung.getSamsungDetails();
+        samsung.getMultiFeatures();
+        samsung.getCalls();
+        samsung.getPicsClick();
+        samsung.getMathDone();
+        samsung.getVideoRecording();
+        samsung.getVideos();
+        samsung.getSongs();
     }
 }
--- src/inclass/mar18/MultiInterface/Smartphone.java
+++ src/inclass/mar18/MultiInterface/Smartphone.java
@@ -5,21 +5,24 @@ public class Smartphone extends MusicPlayer implements Calculator, Camera, Telep
 
     @Override
     public void getMathDone() {
+        System.out.println(" solve maths problem ");
 
     }
 
     @Override
     public void getPicsClick() {
 
+        System.out.println(" 20 magapixel camera ");
     }
 
     @Override
     public void getVideoRecording() {
-
+        System.out.println(" 4k video recording ");
     }
     @Override
     public  void getCalls(){
 
+        System.out.println( " audio and video calls ");
     }
 
     void getMultiFeatures(){
